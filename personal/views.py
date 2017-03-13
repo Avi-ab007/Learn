2359e4e85403134598737197505ebd6d91a9642f
[Avi-ab007] <[avinash.ab007@gmail.com]>
2017-03-13 19:58:40 +0530

    Proceeding with Bootstrap

---
+++ personal/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def index(request):
+	return render(request, 'personal/home.html')
+
+
+def contact(request):
+	return render(request, 'personal/basic.html', 'content': ['Email me if you would like to contact me', 'bavinash@student.nitw.ac.in'])
