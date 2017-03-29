6a2565db9755b97acbd6989f070ba1a0169db5c2
[Avi-ab007] <[avinash.ab007@gmail.com]>
2017-03-29 19:16:54 +0530

    Added Generic views

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ from .models import Post
 app_name = 'blog' 
 urlpatterns = [
 	url(r'^add_post/$', views.add_post, name="add_post"),
-	url(r'^$', ListView.as_view(queryset = Post.objects.all().order_by("-date")[:25], template_name = 'blog/blog.html')),
-	url(r'^(?P<pk>[0-9]+)/$', DetailView.as_view(model = Post, template_name = 'blog/post.html')),
+	url(r'^$', views.postList.as_view(), name='postlist'),
+	url(r'^(?P<pk>[0-9]+)/$', views.blogDetail.as_view(), name='blogdetail'),
 ]
+
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from .models import Post
 from django.http import HttpResponse
@@ -13,3 +13,15 @@ def add_post(request):
 		return render(request, 'blog/blog.html')
 	context = {"form": form}
 	return render(request, 'blog/add_post.html', context)
+
+
+class postList(generic.ListView):
+	template_name = 'blog/blog.html'
+
+	def get_queryset(self):
+		return Post.objects.all().order_by("-date")[:25]
+
+
+class blogDetail(generic.DetailView):
+	model = Post
+	template_name = 'blog/post.html'
